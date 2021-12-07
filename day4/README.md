97bd3ae0959e073018f0e4ca49b82cfeaaa96e19
Haofeng Lei <haofeng.lei@uber.com>
2021-12-07 22:33:01 +0100

    Day 4.2 solution

--- day4/README.md
+++ day4/README.md
@@ -2,13 +2,18 @@
 
 Solved with C#. To be more specific, [.NET 6.0](https://dotnet.microsoft.com/en-us/download/dotnet).
 
-This is my first time coding in C# so the code is probably not idiomatic nor optimal.
+This is my first time coding in C# so the code is probably not idiomatic or optimal.
 
 To run:
 
-`dotnet run prob1/ < path/to/input_data`
+`dotnet run --project prob1 < path/to/input_data`
 
 
 ## Problem 2 of Day 4
 
+C# again.
+
+To run:
+
+`dotnet run --project prob2 < path/to/input_data`
 
---
+++ day4/prob2/Program.cs
@@ -0,0 +1,89 @@
+﻿
+var firstLine = Console.ReadLine();
+if (firstLine == null) {
+  Console.Error.WriteLine("Expected first line to have numbers to draw but is empty");
+  Environment.Exit(1);
+}
+var numbersToDraw = firstLine.Split(',').Select(str => Int32.Parse(str)).ToArray();
+
+// Skip next blank line
+Console.ReadLine();
+
+var boards = new List<Board>();
+string? line;
+while ((line = Console.ReadLine()) != null) {
+  string currentBoard = "";
+  while (line != null && line.Length > 0) {
+    currentBoard = currentBoard + " " + line;
+    line = Console.ReadLine();
+  }
+  boards.Add(new Board(currentBoard.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(str => Int32.Parse(str)).ToArray()));
+}
+
+Board? lastBingoBoard = null;
+int? lastNum = null;
+
+foreach (var num in numbersToDraw) {
+  var found = new List<Board>();
+  foreach (var board in boards) {
+    if (board.drawNumber(num)) {
+      lastBingoBoard = board;
+      lastNum = num;
+      found.Add(board);
+    }
+  }
+  boards = boards.Except(found).ToList();
+  if (boards.Count == 0) {
+    break;
+  }
+}
+
+if (lastBingoBoard != null) {
+  Console.WriteLine(lastBingoBoard.getUnmarkedNumbers().Sum() * lastNum);
+}
+
+class Board {
+  Dictionary<int, (int, int)> numberToPos = new Dictionary<int, (int, int)>();
+
+  int[] rowBitmap = new int[5];
+  int[] colBitmap = new int[5];
+
+  public Board(int[] boardNumbers) {
+    if (boardNumbers.Length != 25) {
+      throw new SystemException("A board must have exactly 25 numbers");
+    }
+    int row = 0;
+    int col = 0;
+    foreach (var num in boardNumbers) {
+      numberToPos.Add(num, (row, col));
+      if (++col == 5) {
+        row++;
+        col = 0;
+      };
+    }
+  }
+
+  public bool drawNumber(int num) {
+    if (!numberToPos.ContainsKey(num)) {
+      return false;
+    }
+
+    var (row, col) = numberToPos[num];
+    numberToPos.Remove(num);
+    rowBitmap[row] |= 1 << col;
+    if (rowBitmap[row] == 0x1f) {
+      // Bingo
+      return true;
+    }
+    colBitmap[col] |= 1 << row;
+    if (colBitmap[col] == 0x1f) {
+      // Bingo
+      return true;
+    }
+    return false;
+  }
+
+  public IEnumerable<int> getUnmarkedNumbers() {
+    return numberToPos.Keys;
+  }
+}
